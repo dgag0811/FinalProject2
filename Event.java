40c8fda93a319a7ac04ab746ea3a1ec112fee8f9
Divya Garg <dgag0811@gmail.com>
2016-07-07 18:11:22 +0530

    Update Event.java

--- Event.java
+++ Event.java
@@ -6,70 +6,61 @@ import java.sql.Time;
  * Created by monikagarg on 6/26/2016.
  */
 
-public class Event {
-    private String title,description,venue,attendees,time;
-    private int image;
-
-
+    public class Event {
+        private String date,title,time,venue,description,attendees;
     public Event() {
     }
-
-    public Event(String title, String description,String venue, String attendees, String time, int image) {
+    public Event(String date, String title, String description,String venue, String attendees, String time) {
+        this.date=date;
         this.title = title;
         this.time = time;
         this.venue = venue;
         this.description = description;
         this.attendees = attendees;
-        this.image= image;
+    }
+    public String getDate()
+    {
+        return date;
+    }
+    public void setDate(String date) {
+        this.date = date;
     }
 
     public String getTitle()
     {
         return title;
     }
-
     public void setTitle(String title) {
         this.title = title;
-    }
-
-    public String getTime() {
-        return time;
-    }
-
-    public void setTime(String time)
-    {
-        this.time = time;
-    }
-
-    public String getVenue() {
-        return venue;
-    }
-
-    public void setVenue(String venue) {
-        this.venue = venue;
     }
 
     public String getDescription() {
         return description;
     }
-
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public String getVenue() {
+        return venue;
+    }
+    public void setVenue(String venue) {
+        this.venue = venue;
     }
 
     public String getAttendees() {
         return attendees;
     }
-
     public void setAttendees(String attendees) {
         this.attendees = attendees;
     }
 
-    public int getImage() {
-        return image;
+    public String getTime() {
+        return time;
+    }
+    public void setTime(String time)
+    {
+        this.time = time;
     }
 
-    public void setImage(int image) {
-        this.image = image;
-    }
 }
