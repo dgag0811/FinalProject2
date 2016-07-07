44ad6aa7a5a7d425d9a66a6b78cada98bdb591b6
Divya Garg <dgag0811@gmail.com>
2016-07-07 18:10:01 +0530

    Update DatabaseHandler.java

--- DatabaseHandler.java
+++ DatabaseHandler.java
@@ -32,38 +32,62 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     private String CREATE_LOGIN_TABLE = "CREATE TABLE " + UtilConstants.LOGIN_TABLE_NAME + " ( "
             + UtilConstants.NAME + " TEXT, " + UtilConstants.USER_NAME + " TEXT PRIMARY KEY, "
             + UtilConstants.PASSWORD + " TEXT ) ";
+    
+     private String CREATE_EVENT_TABLE = "CREATE TABLE " + UtilConstants.EVENT_TABLE_NAME + " ( "
+            + UtilConstants.DATE + " TEXT, "
+            + UtilConstants.no + " TEXT PRIMARY KEY AUTOINCREMENT, "
+            +UtilConstants.TITLE + " TEXT, "
+            +UtilConstants.VENUE + " TEXT, "
+            +UtilConstants.TIME + " TEXT, "
+            +UtilConstants.ATTENDEES+" TEXT, "
+            + UtilConstants.DESCRIPTION + " TEXT ) ";
 
-    private String CREATE_EVENT_TABLE = "CREATE TABLE " + UtilConstants.EVENT_TABLE_NAME + " ( " + UtilConstants.DATE +
-            " TEXT, " + UtilConstants.no + " TEXT, "+UtilConstants.TITLE + " TEXT, "
-            +UtilConstants.VENUE + " TEXT, "+UtilConstants.TIME + " TEXT, "+UtilConstants.ATTENDEES
-            +UtilConstants.IMAGE + " IMAGE, "+ " TEXT, " + UtilConstants.DESCRIPTION + " TEXT ) ";
+    private String CREATE_TIME_TABLE = "CREATE TABLE " + UtilConstants.TIME_TABLE_NAME + " ( "
+            + UtilConstants.DAY + " TEXT, "
+            + UtilConstants.COURSE + " TEXT, "
+            + UtilConstants.TYPE + " TEXT, "
+            + UtilConstants.VENUE + " TEXT, "
+            + UtilConstants.PROFESSOR + " TEXT "
+            + UtilConstants.FROMTIME + " TEXT, "
+            + UtilConstants.TOTIME + " TEXT, "
+            + UtilConstants.ID + " INTEGER PRIMARY KEY AUTOINCREMENT) ";
 
-    private String CREATE_NOTE_TABLE = "CREATE TABLE " + UtilConstants.NOTES_TABLE_NAME+ " ( " + UtilConstants.DESCRIPTION_NOTE +
-            " TEXT, "+ UtilConstants.IMAGE_NOTE + " IMAGE, "+ UtilConstants.note_index+ " TEXT ) ";
+    private String CREATE_MEAL_TABLE = "CREATE TABLE " + UtilConstants.MEAL_TABLE_NAME + " ( "
+            + UtilConstants.MealDAY + " TEXT PRIMARY KEY, "
+            + UtilConstants.BREAKFAST + " TEXT, "
+            + UtilConstants.DINNER + " TEXT, "
+            + UtilConstants.LUNCH + " TEXT)";
+
+    private String CREATE_NOTE_TABLE = "CREATE TABLE " + UtilConstants.NOTES_TABLE_NAME+ " ( "
+            + UtilConstants.DESCRIPTION_NOTE + " TEXT, "
+            + UtilConstants.IMAGE_NOTE + " BLOB, "
+            + UtilConstants.note_index+ " TEXT PRIMARY KEY AUTOINCREMENT ) ";
 
 
     public DatabaseHandler(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
+
     @Override
     public void onCreate(SQLiteDatabase db) {
-  /*      db.execSQL("create table" + TABLE_NAME + "(" +
-                COL_1 + " INTEGER PRIMARY KEY," + COL_2 + " DATE" + COL_3 + "TEXT" + COL_4 + "TEXT" + COL_5 + "TEXT" + COL_6 + "TEXT"
-                + COL_7 + " TEXT" + ")");
-*/
         db.execSQL(CREATE_LOGIN_TABLE);
         db.execSQL(CREATE_EVENT_TABLE);
+        db.execSQL(CREATE_TIME_TABLE);
+        db.execSQL(CREATE_MEAL_TABLE);
         db.execSQL(CREATE_NOTE_TABLE);
+
     }
+
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISIS" + CREATE_LOGIN_TABLE);
-        db.execSQL("DROP TABLE IF EXISIS" + CREATE_EVENT_TABLE);
-        db.execSQL("DROP TABLE IF EXISIS" + CREATE_NOTE_TABLE);
+        db.execSQL("DROP TABLE IF EXISTS" + CREATE_LOGIN_TABLE);
+        db.execSQL("DROP TABLE IF EXISTS" + CREATE_EVENT_TABLE);
+        db.execSQL("DROP TABLE IF EXISTS" + CREATE_TIME_TABLE);
+        db.execSQL("DROP TABLE IF EXISTS" + CREATE_MEAL_TABLE);
+        db.execSQL("DROP TABLE IF EXISTS" + CREATE_NOTE_TABLE);
 
         onCreate(db);
     }
-
-
 }
+
